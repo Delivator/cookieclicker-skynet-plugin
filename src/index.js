0e3494498c7ae0ea0d5978340a0e11420cf744cd
Delivator <info@delivator.me>
2020-11-07 16:45:33 +0100

    Expose skynet-js via window.Skynet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import { SkynetClient, keyPairFromSeed } from "skynet-js";
+import * as Skynet from "skynet-js";
 
-const client = new SkynetClient();
-const { publicKey, privateKey } = keyPairFromSeed("this seed should be fairly long for security");
-
-const dataKey = "myApp";
-
-async function getJSONExample() {
-  try {
-    const { data, revision } = await client.db.getJSON(publicKey, dataKey);
-  } catch (error) {
-    console.log(error);
-  }
-}
+window.Skynet = Skynet;
